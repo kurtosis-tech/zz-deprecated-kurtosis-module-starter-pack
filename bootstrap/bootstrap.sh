caba754986ea1e7d1a3a2150c72e2cb12eb636c1
mieubrisse <mieubrisse@gmail.com>
2021-08-26 15:17:45 -0500

    Add Typescript bootstrapping

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -147,7 +147,7 @@ Quickstart steps:
     1. Write the functionality of your Kurtosis Lambda inside your implementation of the \`KurtosisLambda.execute\` method by using the serialized parameters (validating & sanitizing the parameters as necessary)
     1. Write an implementation of \`KurtosisLambdaConfigurator\` that accepts configuration parameters and produces an instance of your custom Kurtosis Lambda
     1. Edit the main file and replace the example \`KurtosisLambdaConfigurator\` with your own implementation that produces your custom Lambda
-    1. Run \`path/to/your/code/repos/scripts/build.sh\` to package your Kurtosis Lambda into a Docker image that can be used inside Kurtosis
+    1. Run \`scripts/build.sh\` to package your Kurtosis Lambda into a Docker image that can be used inside Kurtosis
 EOF
 if [ "${?}" -ne 0 ]; then
   echo "Error: Could not write README file to '${output_readme_filepath}'" >&2
