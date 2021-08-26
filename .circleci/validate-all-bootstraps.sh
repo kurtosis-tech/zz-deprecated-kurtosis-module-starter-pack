30f51f6775ed5060491e0a09a0e538554733426a
mieubrisse <mieubrisse@gmail.com>
2021-08-26 15:20:41 -0500

    Add error-checking for validate-all-bootstraps

--- .circleci/validate-all-bootstraps.sh
+++ .circleci/validate-all-bootstraps.sh
@@ -14,11 +14,14 @@ BUILD_SCRIPT_FILENAME="build.sh"
 GIT_USER_EMAIL_PROPERTY="user.email"
 GIT_USER_NAME_PROPERTY="user.name"
 
+MISSING_BOOTSTRAP_FLAGS_KEY="MISSING"
+
 # Bootstrapping normally requires input from STDIN, but we can set
 #  certain variables so this isn't required for CI
 # NOTE: This won't handle flag values that contain spaces, though it can handle multiple flags separated by a space
 declare -A CUSTOM_LANG_BOOTSTRAP_FLAGS 
 CUSTOM_LANG_BOOTSTRAP_FLAGS[golang]="GO_NEW_MODULE_NAME=github.com/test/test-module"
+# CUSTOM_LANG_BOOTSTRAP_FLAGS[typescript]=""  # No extra Typescript bootstrapping flags needed
 
 
 
@@ -68,7 +71,11 @@ for lang in $(cat "${root_dirpath}/${SUPPORTED_LANGS_FILENAME}"); do
     echo "Bootstrapping and running ${lang} Kurtosis Lambda..."
     output_dirpath="$(mktemp -d)"
     kurtosis_lambda_image="bootstrap-kurtosis-lambda-${lang}-image"
-    lang_specific_vars_to_set="${CUSTOM_LANG_BOOTSTRAP_FLAGS[${lang}]}"
+    lang_specific_vars_to_set="${CUSTOM_LANG_BOOTSTRAP_FLAGS[${lang}]:-"${MISSING_BOOTSTRAP_FLAGS_KEY}"}"
+    if [ "${lang_specific_vars_to_set}" == "${MISSING_BOOTSTRAP_FLAGS_KEY}" ]; then
+        echo "Error: No custom bootstrap flas were defined for ${lang}; this bootstrapping script needs to be updated with them" >&2
+        exit 1
+    fi
     command="${lang_specific_vars_to_set} ${bootstrap_script_filepath} ${lang} ${output_dirpath} ${kurtosis_lambda_image}"
     if ! eval "${command}"; then
         echo "Error: Bootstrapping ${lang} Kurtosis Lambda failed" >&2
